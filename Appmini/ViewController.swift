02f3da4040ee8356057b141b7426e907d65c47f3
yeonwoo <mamen7624@gmail.com>
2024-08-09 18:35:02 +0900

    Initial Commit

---
+++ Appmini/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Appmini
+//
+//  Created by Woo on 8/9/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
